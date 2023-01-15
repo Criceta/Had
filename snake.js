7c507b015a16dd8c2ba7c7fc0fae55fb64871e9a
Criceta <eva.kospachova@student.pedf.cuni.cz>
2023-01-15 11:46:05 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -10,6 +10,9 @@ var context;
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
 
+var velocityX = 0;
+var velocityY = 0;
+
 //food
 var foodX;
 var foodY;
@@ -21,7 +24,9 @@ window.onload = function() {
     context = board.getContext("2d");
 
     placeFood();
-    update();
+    document.addEventListener("keyup", changeDirection);
+    //update();
+    setInterval(update, 1000/10); //100 milliseconds
 }
 
 function update() {
@@ -29,10 +34,31 @@ function update() {
     context.fillRect(0, 0, board.width, board.height);
 
     context.fillStyle="lime";
+    snakeX += velocityX;
+    snakeY += velocityY;
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
 
     context.fillStyle="red";
     context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
 }
 
 
