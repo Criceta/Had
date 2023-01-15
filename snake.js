5c96d491e547c98ac73ef0d2032c0bdda427099c
Criceta <eva.kospachova@student.pedf.cuni.cz>
2023-01-15 12:17:09 +0100

    Update snake.js

--- snake.js
+++ snake.js
@@ -19,6 +19,8 @@ var snakeBody = [];
 var foodX;
 var foodY;
 
+var gameOver = false;
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
@@ -32,6 +34,10 @@ window.onload = function() {
 }
 
 function update() {
+    if (gameOver) {
+        return;
+    }
+
     context.fillStyle="black";
     context.fillRect(0, 0, board.width, board.height);
 
@@ -56,6 +62,19 @@ function update() {
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
     for (let i = 0; i < snakeBody.length; i++) {
         context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+    //game over conditions
+    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
+        gameOver = true;
+        alert("Game Over");
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
+            gameOver = true;
+            alert("Game Over");
+        }
     }
 }
 
