f981e69af2884b48ea0791733c500ae9a0d0cc67
Criceta <eva.kospachova@student.pedf.cuni.cz>
2023-02-04 18:57:19 +0100

    Update snakeModel.js

--- snakeModel.js
+++ snakeModel.js
@@ -12,56 +12,6 @@ class Had {
         this.text = "ahoj";
     }
 
-    /*napis () {
-        this.velocityX = 0;
-        this.velocityY = -1;
-    }*/
-    
-    
-    /*up (event) {
-        //this.text = "Jsem tu";
-        //document.write(this.text);
-        let key = event.code;
-        //document.getElementById("echo").innerHTML = key;
-        
-        //document.write(key);
-
-        if (key == "ArrowUp") {
-            gameOver = true;
-            this.text = "Jsem tu";
-            document.write(this.text);
-            this.text = "Pravda";
-
-            this.velocityX = 0;
-            this.velocityY = -1;
-
-        }
-        else {
-            //this.velocityX = 1;
-            //this.velocityY = 0;
-            this.text = "Nepravda";
-        }
-        document.getElementById("echo").innerHTML = this.text;
-    }*/
-
-    /*down (event) {
-        let key = event.code;
-        if (key == "ArrowUp") {
-            this.text = "Pravda";
-
-            this.velocityX = 0;
-            this.velocityY = -1;
-
-        }
-        else {
-            //this.velocityX = 1;
-            //this.velocityY = 0;
-            this.text = "Nepravda";
-        }
-        document.getElementById("echo").innerHTML = this.text;
-    }*/
-    
-
     pohyb (event) {
         let key = event.code;
         if (key == "ArrowUp" && this.velocityY != 1) {
@@ -91,60 +41,11 @@ class Had {
 
         velX = this.velocityX;
         velY = this.velocityY;
-        //return this.velocityY;
 
-    }
-
-    
-        //document.write(this.text);
-        //this.velocityX = 3;
-        
-        /*document.addEventListener("ArrowUp")
-            this.text = "Ano";
-            document.write(this.text);
-        }else if (document.addEventListener("ArrowUp")) {
-            this.text = "Ne";
-            document.write(this.text);
-        }
-        else {
-            document.write(this.text);
-        }*/
-        //this.text;
-    
-
-    changeDirection(e) {
-        //document.getElementById("echo").innerHTML = kobra.snakeX;
-        if (e.code == "ArrowUp" && this.velocityY != 1) {
-            this.velocityX = 0;
-            this.velocityY = -1;
-        }
-        else if (e.code == "ArrowDown" && this.velocityY != -1) {
-            this.velocityX = 0;
-            this.velocityY = 1;
-        }
-        else if (e.code == "ArrowLeft" && this.velocityX != 1) {
-            this.velocityX = -1;
-            this.velocityY = 0;
-        }
-        else if (e.code == "ArrowRight" && this.velocityX != -1) {
-            this.velocityX = 1;
-            this.velocityY = 0;
-        }
-        
     }
     
 
 }        
-
-
-    
-    
-    //changeDirection(e) 
-
-
-
-
-
 
 class jidlo {
     
